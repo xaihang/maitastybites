6be21b6c6cf61f8ccf17252b031c08aa096d3221
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-11 23:05:18 -0600

    added css file for user page component

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import './UserPage.css';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -9,7 +9,8 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+<button className="add-recipe-button"></button>
+<button className="view-saved-recipes-button"></button>
     </div>
   );
 }
