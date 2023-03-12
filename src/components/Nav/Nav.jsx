84c5f41e31e243065aa990a8c3c22b0272ce0c1b
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-11 18:07:53 -0600

    added nav styles with mui

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,46 +1,178 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
+import { styled } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Home", "Info"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const NavTitle = styled("h2")({
+  fontSize: "24px",
+  fontWeight: 700,
+  color: "#f2f2f2",
+  paddingLeft: "10px",
+  margin: 0,
+});
+
+const NavLink = styled(Link)({
+  display: "inline-block",
+  color: "#f2f2f2",
+  backgroundColor: "#00acb0",
+  textAlign: "center",
+  padding: "24px 10px",
+  textDecoration: "none",
+  fontSize: "15px",
+  border: "none",
+  cursor: "pointer",
+  outline: 0,
+  "&:hover": {
+    backgroundColor: "#008183",
+  },
+});
 
 function Nav() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
   const user = useSelector((store) => store.user);
 
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">MaiTastyBites</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+            sx={{ display: { xs: "block", md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component={Link}
+            to="/home"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <NavTitle>MaiTastyBites</NavTitle>
+          </Typography>
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        {/* <Link className="navLink" to="/about">
-          About
-        </Link> */}
-      </div>
-    </div>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <NavLink key={page} to={`/${page.toLowerCase()}`}>
+                {page}
+              </NavLink>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            {user.id && (
+              <>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="User Avatar"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )}
+            {!user.id && (
+              <NavLink className="navLink" to="/login">
+                Login / Register
+              </NavLink>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+      <Menu
+        id="nav-menu"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+      >
+        {pages.map((page) => (
+          <MenuItem
+            key={page}
+            onClick={handleCloseNavMenu}
+            component={Link}
+            to={`/${page.toLowerCase()}`}
+          >
+            {page}
+          </MenuItem>
+        ))}
+      </Menu>
+    </AppBar>
   );
 }
 
