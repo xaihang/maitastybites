a13a97886ee69cd8b7c1caa3ae169a2bc31aada1
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-13 16:03:29 -0500

    details page added useparams() for id and dispatch action get_recipe_by_id to saga; added useSelect to get recipe by id in store; in return statement added logics to display result

--- src/components/DetailsRecipePage/DetailsRecipePage.jsx
+++ src/components/DetailsRecipePage/DetailsRecipePage.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
-export default function DetailsRecipePage() {
+const DetailsRecipePage = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const recipe = useSelector((state) => state.recipesAll.find((r) => r.recipeID === id));
+
+  useEffect(() => {
+    dispatch({ type: 'GET_RECIPE_BY_ID', payload: id });
+  }, [dispatch, id]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>DetailsRecipePage</div>
-  )
-}
+    <div>
+      <h1>{recipe.recipename}</h1>
+      <img src={recipe.url} alt={recipe.recipename} />
+      <p>{recipe.description}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        {recipe.ingredients.split('\n').map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h2>Directions</h2>
+      <ol>
+        {recipe.direction.split('\n').map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default DetailsRecipePage;
