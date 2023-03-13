9bbbea6aa748c5fc14cc3c4624ce85365e30e523
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-13 16:12:32 -0500

    recipelistgallery added click handler to recipe image so when clicked it will nav user to details about item

--- src/components/LandingPage/RecipeListGallery.jsx
+++ src/components/LandingPage/RecipeListGallery.jsx
@@ -2,15 +2,25 @@ import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 export default function RecipeListGallery() {
+  const history = useHistory();
   const { recipesAll } = useSelector((state) => state.recipe);
+
+  const handleClick = (recipeID) => {
+    history.push(`/recipes/${recipeID}`);
+  };
 
   return (
     <div>
       <ImageList sx={{ width: "100%" }} variant="woven" cols={3} gap={8}>
         {recipesAll.map((recipe) => (
-          <ImageListItem key={recipe.recipeID} className="ImageList-item">
+          <ImageListItem
+            key={recipe.recipeID}
+            className="ImageList-item"
+            onClick={() => handleClick(recipe.recipeID)}
+          >
             <div className="overlay"></div>
             <img src={recipe.url} alt={recipe.recipename} loading="lazy" />
             <ImageListItemBar
