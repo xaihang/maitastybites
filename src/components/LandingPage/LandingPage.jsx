514a88478c7f0f50685707e786058f007742e6fd
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-13 19:18:26 -0500

    signup btn added to hero img

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,6 +4,41 @@ import "./LandingPage.css";
 import { useDispatch, useSelector } from "react-redux";
 import RecipeListGallery from "./RecipeListGallery";
 import heroImage from '../LandingPage/hero.png';
+import Button from "@mui/material/Button";
+
+const FreeSignUpButton = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/registration');
+  };
+
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        backgroundColor: "red",
+        color: "white",
+        border: "none",
+        padding: "10px 20px",
+        marginTop: "10px",
+        transition: "all 0.3s ease",
+        borderRadius: 25,
+        "&:hover": {
+          backgroundColor: "darkred",
+        },
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        zIndex: 2,
+      }}
+      onClick={handleClick}
+    >
+      Free Sign up!
+    </Button>
+  );
+};
 
 function LandingPage() {
   const history = useHistory();
@@ -20,6 +55,7 @@ function LandingPage() {
   return (
     <div className="container">
       <img src={heroImage} alt="hero image" />
+      <FreeSignUpButton />
       <RecipeListGallery />
     </div>
   );
