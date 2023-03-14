f94f29bbce03efa0d3b2da529badc87c73daffb7
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-14 16:28:12 -0500

    saga added for update_recipe

--- src/redux/sagas/recipe.saga.jsx
+++ src/redux/sagas/recipe.saga.jsx
@@ -1,60 +1,80 @@
-import { call, put, takeLatest, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
+import { call, put, takeLatest, takeEvery } from "redux-saga/effects";
+import axios from "axios";
 
 function* getUserRecipes() {
-  console.log('getUserRecipes')
+  console.log("getUserRecipes");
   try {
-    const response = yield call(axios.get, '/api/recipe/user');
-    console.log('response', response)
-    yield put({ type: 'GET_USER_RECIPES_SUCCESS', payload: response.data });
+    const response = yield call(axios.get, "/api/recipe/user");
+    console.log("response", response);
+    yield put({ type: "GET_USER_RECIPES_SUCCESS", payload: response.data });
   } catch (error) {
-    console.log('Error getting recipes:', error);
-    yield put({ type: 'GET_USER_RECIPES_ERROR' });
+    console.log("Error getting recipes:", error);
+    yield put({ type: "GET_USER_RECIPES_ERROR" });
   }
 }
 
 function* getAllRecipes() {
-  console.log('getAllRecipes')
+  console.log("getAllRecipes");
   try {
-    const response = yield call(axios.get, '/api/recipe');
-    console.log('response', response)
-    yield put({ type: 'GET_ALL_RECIPES_SUCCESS', payload: response.data });
+    const response = yield call(axios.get, "/api/recipe");
+    console.log("response", response);
+    yield put({ type: "GET_ALL_RECIPES_SUCCESS", payload: response.data });
   } catch (error) {
-    console.log('Error getting all recipes:', error);
-    yield put({ type: 'GET_ALL_RECIPES_ERROR' });
+    console.log("Error getting all recipes:", error);
+    yield put({ type: "GET_ALL_RECIPES_ERROR" });
   }
 }
 
 function* addRecipe(action) {
   try {
-    yield call(axios.post, '/api/recipe', action.payload);
-    yield put({ type: 'ADD_RECIPE_SUCCESS' });
-    yield put({ type: 'GET_USER_RECIPES' }); 
+    yield call(axios.post, "/api/recipe", action.payload);
+    yield put({ type: "ADD_RECIPE_SUCCESS" });
+    yield put({ type: "GET_USER_RECIPES" });
   } catch (error) {
-    console.log('Error adding recipe:', error);
-    yield put({ type: 'ADD_RECIPE_ERROR' });
+    console.log("Error adding recipe:", error);
+    yield put({ type: "ADD_RECIPE_ERROR" });
   }
 }
 
 function* getRecipeById(action) {
   try {
     const response = yield call(axios.get, `/api/recipe/${action.payload}`);
-    yield put({ type: 'GET_SELECTED_RECIPE_SUCCESS', payload: response.data });
+    yield put({ type: "GET_SELECTED_RECIPE_SUCCESS", payload: response.data });
   } catch (error) {
-    console.log('Error getting recipe by id:', error);
-    yield put({ type: 'GET_RECIPE_BY_ID_ERROR' });
+    console.log("Error getting recipe by id:", error);
+    yield put({ type: "GET_RECIPE_BY_ID_ERROR" });
   }
 }
 
+function* updateRecipe(action) {
+  try {
+    const { recipename, description, ingredients, direction, url } =
+      action.payload;
+    const recipeID = action.payload.recipeID;
+    yield call(axios.put, `/api/recipe/${recipeID}`, {
+      recipename,
+      description,
+      ingredients,
+      direction,
+      url,
+    });
+    yield put({ type: "UPDATE_RECIPE_SUCCESS" });
+    yield put({ type: "GET_USER_RECIPES" }); // refresh the user's recipes
+  } catch (error) {
+    console.log("Error updating recipe:", error);
+    yield put({ type: "UPDATE_RECIPE_ERROR" });
+  }
+}
 
 function* recipeSaga() {
-  yield takeLatest('ADD_RECIPE', addRecipe);
-  yield takeEvery('GET_ALL_RECIPES', getAllRecipes);
-  yield takeEvery('GET_USER_RECIPES', function* () {
-    console.log('GET_USER_RECIPES action dispatched');
+  yield takeLatest("ADD_RECIPE", addRecipe);
+  yield takeEvery("GET_ALL_RECIPES", getAllRecipes);
+  yield takeEvery("GET_USER_RECIPES", function* () {
+    console.log("GET_USER_RECIPES action dispatched");
     yield call(getUserRecipes);
-    console.log('GET_USER_RECIPES finished');
+    console.log("GET_USER_RECIPES finished");
+    yield takeLatest("UPDATE_RECIPE", updateRecipe);
   });
-  yield takeLatest('GET_RECIPE_BY_ID', getRecipeById);
+  yield takeLatest("GET_RECIPE_BY_ID", getRecipeById);
 }
 export default recipeSaga;
