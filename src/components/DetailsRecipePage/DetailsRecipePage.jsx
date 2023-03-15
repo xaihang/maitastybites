d93c1074fff6b2544358a52033cf151c79d8ed75
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-15 13:12:57 -0500

    resize img, recipes info in different land throughout the box container; styles display, margins, height, width

--- src/components/DetailsRecipePage/DetailsRecipePage.jsx
+++ src/components/DetailsRecipePage/DetailsRecipePage.jsx
@@ -19,24 +19,33 @@ const recipe = useSelector((state) => state.recipe.selectedRecipe)
 
   return (
     <div className="parent-details-container">
-    <div className="child-details-container">
-      <h1>{recipe.recipename}</h1>
-      <p>{recipe.description}</p>
-      <img src={recipe.url} alt={recipe.recipename} />
-     
-      <h2>Ingredients</h2>
-      <ul>
-        {recipe.ingredients.split('\n').map((ingredient, index) => (
-          <li key={index}>{ingredient}</li>
-        ))}
-      </ul>
-      <h2>Directions</h2>
-      <ol>
-        {recipe.direction.split('\n').map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
-      </ol>
-    </div>
+      <div className="child-details-container">
+        <div className="recipe-details">
+          <div className="recipe-image">
+            <img src={recipe.url} alt={recipe.recipename} />
+          </div>
+          <div className="recipe-info">
+            <h1>{recipe.recipename}</h1>
+            <p>{recipe.description}</p>
+          </div>
+        </div>
+        <div className="recipe-ingredients">
+          <h2>Ingredients</h2>
+          <ul>
+            {recipe.ingredients.split('\n').map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="recipe-directions">
+          <h2>Directions</h2>
+          <ol>
+            {recipe.direction.split('\n').map((step, index) => (
+              <li key={index}>{step}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 };
