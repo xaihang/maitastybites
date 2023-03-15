ab2045359c161c293af0264e8f57111dbb9a0473
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-15 15:14:58 -0500

    import use selector hook

--- src/components/DetailsRecipePage/RecipeCommentForm.jsx
+++ src/components/DetailsRecipePage/RecipeCommentForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import CustomButton from "../UserPage/CustomButton";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Box,
   Button,
