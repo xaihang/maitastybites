0ca777d7e34dbde462a53931e0045c7c8aa7bd3a
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-16 20:13:49 -0500

    added save and saved btn toogle and design to overlay

--- src/components/LandingPage/RecipeListGallery.jsx
+++ src/components/LandingPage/RecipeListGallery.jsx
@@ -28,19 +28,21 @@ export default function RecipeListGallery({recipesList}) {
       <ImageList sx={{ width: "100%" }} variant="woven" cols={3} gap={8}>
         {recipesList?.map((recipe) => (
           <ImageListItem key={recipe.recipeID} className="ImageList-item">
-            <div className="overlay"></div>
-            <img src={recipe.url} alt={recipe.recipename} loading="lazy" />
-            <ImageListItemBar
-              title={recipe.recipename}
-              subtitle={recipe.description}
-              actionIcon={
-                <CustomButton
+              
+            <div className="overlay">
+            <CustomButton
                   onClick={() => handleSaveRecipe(recipe.recipeID)}
                   className={savedRecipes.includes(recipe.recipeID) ? "saveEdBtn" : "saveBtn"}
                 >
                   {savedRecipes.includes(recipe.recipeID) ? "Saved" : "Save"}
                 </CustomButton>
-              }
+            </div>
+
+            <img src={recipe.url} alt={recipe.recipename} loading="lazy" />
+            <ImageListItemBar
+              title={recipe.recipename}
+              subtitle={recipe.description}
+           
             />
           </ImageListItem>
         ))}
