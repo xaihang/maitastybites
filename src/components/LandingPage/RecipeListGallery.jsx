2372468743f93b2c519f4e532fead564d8fdff4c
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-16 20:29:36 -0500

    setup alert for saved handler

--- src/components/LandingPage/RecipeListGallery.jsx
+++ src/components/LandingPage/RecipeListGallery.jsx
@@ -1,14 +1,20 @@
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
-import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import CustomButton from "../UserPage/CustomButton";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function RecipeListGallery({ recipesList }) {
   const history = useHistory();
   const [savedRecipes, setSavedRecipes] = useState([]);
+  const [submitSuccess, setSubmitSuccess] = useState(false);
 
   const handleClick = (recipeID) => {
     history.push(`/details/${recipeID}`);
@@ -22,6 +28,13 @@ export default function RecipeListGallery({ recipesList }) {
     }
   };
 
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSubmitSuccess(false);
+  };
+
   return (
     <div>
       <ImageList sx={{ width: "100%" }} variant="woven" cols={3} gap={8}>
@@ -29,17 +42,17 @@ export default function RecipeListGallery({ recipesList }) {
           <ImageListItem key={recipe.recipeID} className="ImageList-item">
             <div className="overlay">
               <div className="saveRecipeToggleBtnDisplay">
-              <CustomButton
-                onClick={() => handleSaveRecipe(recipe.recipeID)}
-                className={
-                  savedRecipes.includes(recipe.recipeID)
-                  ? "saveEdBtn"
-                  : "saveBtn"
-                }
+                <CustomButton
+                  onClick={() => handleSaveRecipe(recipe.recipeID)}
+                  className={
+                    savedRecipes.includes(recipe.recipeID)
+                      ? "saveEdBtn"
+                      : "saveBtn"
+                  }
                 >
-                {savedRecipes.includes(recipe.recipeID) ? "Saved" : "Save"}
-              </CustomButton>
-                </div>
+                  {savedRecipes.includes(recipe.recipeID) ? "Saved" : "Save"}
+                </CustomButton>
+              </div>
             </div>
 
             <img src={recipe.url} alt={recipe.recipename} loading="lazy" />
@@ -50,6 +63,20 @@ export default function RecipeListGallery({ recipesList }) {
           </ImageListItem>
         ))}
       </ImageList>
+
+      <Snackbar
+        open={submitSuccess}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Saved successfully!
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
