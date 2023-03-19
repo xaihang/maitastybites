4c9f524eaca2f5f5bbb907e4906d701da452fe7f
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-18 23:02:56 -0500

    clean up code and remove unused components

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import "./LandingPage.css";
 import { useDispatch, useSelector } from "react-redux";
 import RecipeListGallery from "./RecipeListGallery";
 import heroImage from "../LandingPage/hero.png";
-import SignUpButton from "./SignupButton";
+
 
 function LandingPage() {
   const history = useHistory();
@@ -18,28 +18,23 @@ function LandingPage() {
     history.push("/login");
   };
 
-    const { recipesAll } = useSelector((state) => state.recipe);
+  const { recipesAll } = useSelector((state) => state.recipe);
 
   return (
     <>
-        <div className="hero-image-landing-page">
+      <div className="hero-image-landing-page">
         <img src={heroImage} alt="hero image" />
       </div>
-    <div className="container">
-  
-      <div className="rounded-rectangle">
-        <div className="rounded-rectangle-text">
-          <p>Welcome to</p>
-          <h1>Mai Tasty Bites!</h1>
-          {/* <span>Let's get happy cooking together</span> */}
-          {/* <div className="signup-button">
-            <SignUpButton />
-          </div> */}
+      <div className="container">
+        <div className="rounded-rectangle">
+          <div className="rounded-rectangle-text">
+            <p>Welcome to</p>
+            <h1>Mai Tasty Bites!</h1>
+          </div>
         </div>
+        <RecipeListGallery recipesList={recipesAll} />
       </div>
-      <RecipeListGallery recipesList={recipesAll}/>
-    </div>
-          </>
+    </>
   );
 }
 
