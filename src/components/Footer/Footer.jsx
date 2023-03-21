50a28970b7ed78a6b2baae29fc8692c4e7ac65a9
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-20 19:23:27 -0500

    changed favicon.ico img

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; 2023 Mai Tasty Bites. All rights reserved.</footer>;
+  return <footer>&copy; 2023 Mai Tasty Bites! All rights reserved.</footer>;
 }
 
 export default Footer;
